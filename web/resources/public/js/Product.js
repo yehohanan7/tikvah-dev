ecca4d43a705f74509a4fb8ebf0520a17897fdbf
John <yehohanan7@gmail.com>
2013-02-21 05:36:42 +0530

    angularjs prototype

--- web/resources/public/js/Product.js
+++ web/resources/public/js/Product.js
@@ -1,16 +1,51 @@
-function Product($scope) {
+var tikvah = angular.module('tikvah', []);
 
-    $scope.products = [];
+tikvah.controller('ProductCtrl', function ($scope) {
 
-    $scope.create = function () {
-        /*var searchKey = $scope.searchKey;
-         $scope.products.push({id: "1", name: "Mac Book Pro", price: "$2222", available: "ok"},
-         {id: "2", name: "Mac Book Pro", price: "$2222", available: "ok"},
-         {id: "3", name: "Mac Book Pro", price: "$2222", available: "remove"},
-         {id: "4", name: "Mac Book Pro", price: "$2222", available: "remove"});
 
-         $scope.searchKey = '';*/
+    $scope.all = [
+        {id: 1, name: "Mac Book pro", price: "$123456", available: true},
+        {id: 2, name: "Mac Book pro", price: "$123456", available: true},
+        {id: 3, name: "XBOX", price: "$123456", available: false, media: {
+            photos: ['img/gallery/xbox-1.jpg', 'img/gallery/xbox-2.jpg', 'img/gallery/xbox-3.jpg']
+        }},
+        {id: 4, name: "PS3", price: "$123456", available: false}
+    ]
 
+    $scope.results = [];
+
+
+    $scope.find = function () {
+        //var searchKey = $scope.searchKey;
+        $scope.results.push.apply($scope.results, $scope.all);
+        $('#productsModal').modal({})
     }
 
-}
+    $scope.select = function (index) {
+        $scope.selected = $scope.results[index - 1]
+    }
+
+
+    $('.products-typeahead').typeahead(
+        {
+            source: function (typeahead, query) {
+                var results = $.map($scope.all, function (product) {
+                    return product.name;
+                });
+                return results;
+            },
+
+            updater: function (searchKey) {
+                return searchKey;
+
+            }
+        }
+    );
+
+    $(document).ready(function () {
+        $('.carousel').carousel();
+    });
+
+
+})
+
