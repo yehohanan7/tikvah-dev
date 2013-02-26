cac35061efba806d3598ada531c65e882e43cde2
John <yehohanan7@gmail.com>
2013-02-26 07:09:11 +0530

    batch setup

---
+++ batch/project.clj
@@ -0,0 +1,10 @@
+(defproject tikvah/batch "0.1.0-SNAPSHOT"
+  :description "The Batch Layer for the Tikvah project"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [tikvah/commons "0.1.0-SNAPSHOT"]]
+  :source-paths ["src/main/clojure"]
+  :test-paths ["src/test/clojure"]
+  :plugins [])
